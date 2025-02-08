777dab454be2ce7dfdcf609a574d813579299b32
VPeti1 <vpeti011@protonmail.com>
2025-02-08 13:36:15 +0100

    V2 (Added persistent storage and fixed password errors)

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -4,4 +4,6 @@ chmod +x /home/deck/tools/*
 systemctl enable sddm
 useradd -M deck -g deck
 chown -R deck /home/deck
+echo -e "deck\ndeck" | sudo passwd deck
+systemctl enable /etc/systemd/system/pr.service
 loadkeys hu
