8b627e99fa93bd3e89fb0bf52f273a0bf1c405a2
VPeti1 <vpeti011@protonmail.com>
2025-02-08 13:25:20 +0100

    V2 (Updated COWSPACE)

--- profiledef.sh
+++ profiledef.sh
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2034
 
 iso_name="SteamOS"
-iso_label="steamdeck-repair_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="SteamOS <https://store.steampowered.com/steamdeck/>"
+iso_label="downstream-steamdeck-repair_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="VPeti1"
 iso_application="SteamOS Live/Rescue image"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
@@ -14,6 +14,7 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
+#airootfs_image_tool_options=()
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
@@ -26,3 +27,4 @@ file_permissions=(
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
+cowspace_size="5G"
